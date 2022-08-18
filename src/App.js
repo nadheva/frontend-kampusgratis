cd27e56cc6e38742e43a1c03b0c4b3e58e56e870
muhamadazis32 <muhamadazis9a@gmail.com>
2022-08-18 22:37:06 +0700

    update app.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,9 @@ import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to React Router!</h1>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 
