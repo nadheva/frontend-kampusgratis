88d8207d218a1fe273c4afd14c64c66510511929
muhamadazis32 <muhamadazis9a@gmail.com>
2022-08-18 23:10:27 +0700

    [Slicing] Login page - Belum selesai

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
-import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
+
+// page
+import Login from "./pages/Login";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <>
-      <h1>Halaman Home</h1>
-    </>
+    <div>
+      <h1>Home</h1>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/login">Login</Link>
+      </nav>
+    </div>
   );
 }
 
