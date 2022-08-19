41beb3859aef1cd34bde17972b4f0b0a2129eb80
muhamadazis32 <muhamadazis9a@gmail.com>
2022-08-19 10:57:05 +0700

    fix route

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
-import './App.css';
+import "./App.css";
 
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from "react-router-dom";
 
-// import Header from './component/header/Header';
-// import Footer from './component/foooter/Footer';
-
-import Routed from './config/Routed';
+import Routed from "./config/Routed";
 
 function App() {
   return (
     <BrowserRouter>
-      <Route render={props => (
-          <>
-              <Routed/>
-          </>
-      )}/>
+      <Routed />
     </BrowserRouter>
   );
 }
--- src/config/Routed.jsx
+++ src/config/Routed.jsx
@@ -1,29 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch } from "react-router-dom";
 
-import Login from '../pages/Login';
-import Regist from '../pages/Regist';
-import Home from '../pages/Home';
+import Login from "../pages/Login";
+import Regist from "../pages/Regist";
+import Home from "../pages/Home";
 
 const Routed = () => {
-    return (
-        <Switch>
-            <Route
-                path='/'
-                exact
-                component={Home}
-            />
-            <Route
-                path='/login'
-                component={Login}
-            />
-            <Route
-                path='/registration'
-                component={Regist}
-            />
-        </Switch>
-    );
-}
+  return (
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/login" component={Login} />
+      <Route path="/registration" component={Regist} />
+    </Switch>
+  );
+};
 
 export default Routed;
