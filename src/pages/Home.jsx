a0e89eb6f8fa37d3ab9f37399f1186e93c04501e
muhamadazis32 <muhamadazis9a@gmail.com>
2022-08-22 22:40:26 +0700

    fix css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Hero from "../component/landing-page/Hero";
-import Footer from "../component/partials/footer";
+import Footer from "../component/partials/Footer";
 import Header from "../component/partials/Header";
 import Counter from "../component/landing-page/Counter";
 import Fakultas from "../component/landing-page/Fakultas";
