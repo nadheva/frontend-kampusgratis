9bc97030c17481de1d3b6da32fac7c830e7a45b1
muhamadazis32 <muhamadazis9a@gmail.com>
2022-08-22 11:17:43 +0700

    delete slider di mentor

--- src/component/landing-page/Mentor.jsx
+++ src/component/landing-page/Mentor.jsx
@@ -10,58 +10,47 @@ const Mentor = () => {
           </div>
         </div>
 
-        <div className="tiny-slider arrow-round arrow-blur arrow-hover">
-          <div
-            className="row tiny-slider-inner pb-1"
-            data-autoplay="true"
-            data-arrow="true"
-            data-edge="2"
-            data-dots="false"
-            data-items="4"
-            data-items-lg="2"
-            data-items-sm="1"
-          >
-            <div className="col-sm-6 col-lg-3 col-xl-3  action-trigger-hover">
-              <div className="single-person">
-                <div className="person-image">
-                  <img src="./assets/images/mentor/pak-kembar.png" alt="" />
-                </div>
-                <div className="person-info text-center">
-                  <h3 className="full-name">Kembar Cahyono </h3>
-                </div>
+        <div className="row pb-1">
+          <div className="col-sm-6 col-lg-3 col-xl-3">
+            <div className="single-person">
+              <div className="person-image">
+                <img src="./assets/images/mentor/pak-kembar.png" alt="" />
+              </div>
+              <div className="person-info text-center">
+                <h3 className="full-name">Kembar Cahyono </h3>
               </div>
             </div>
+          </div>
 
-            <div className="col-sm-6 col-lg-3 col-xl-3  action-trigger-hover">
-              <div className="single-person">
-                <div className="person-image">
-                  <img src="./assets/images/mentor/pak-alyunas.png" alt="" />
-                </div>
-                <div className="person-info text-center">
-                  <h3 className="full-name">Alyuhanas</h3>
-                </div>
+          <div className="col-sm-6 col-lg-3 col-xl-3">
+            <div className="single-person">
+              <div className="person-image">
+                <img src="./assets/images/mentor/pak-alyunas.png" alt="" />
+              </div>
+              <div className="person-info text-center">
+                <h3 className="full-name">Alyuhanas</h3>
               </div>
             </div>
+          </div>
 
-            <div className="col-sm-6 col-lg-3 col-xl-3  action-trigger-hover">
-              <div className="single-person">
-                <div className="person-image">
-                  <img src="./assets/images/mentor/pak-apud.png" alt="" />
-                </div>
-                <div className="person-info text-center">
-                  <h3 className="full-name">Apud Salapudin</h3>
-                </div>
+          <div className="col-sm-6 col-lg-3 col-xl-3">
+            <div className="single-person">
+              <div className="person-image">
+                <img src="./assets/images/mentor/pak-apud.png" alt="" />
+              </div>
+              <div className="person-info text-center">
+                <h3 className="full-name">Apud Salapudin</h3>
               </div>
             </div>
+          </div>
 
-            <div className="col-sm-6 col-lg-3 col-xl-3  action-trigger-hover">
-              <div className="single-person">
-                <div className="person-image">
-                  <img src="./assets/images/mentor/pak-komala.png" alt="" />
-                </div>
-                <div className="person-info text-center">
-                  <h3 className="full-name">M. Komala Daru </h3>
-                </div>
+          <div className="col-sm-6 col-lg-3 col-xl-3">
+            <div className="single-person">
+              <div className="person-image">
+                <img src="./assets/images/mentor/pak-komala.png" alt="" />
+              </div>
+              <div className="person-info text-center">
+                <h3 className="full-name">M. Komala Daru </h3>
               </div>
             </div>
           </div>
