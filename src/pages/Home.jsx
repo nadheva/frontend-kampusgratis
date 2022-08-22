a769cc8bdb79b9c7d5559c8ef76a6a377eeba09a
muhamadazis32 <muhamadazis9a@gmail.com>
2022-08-22 09:52:54 +0700

    slicing footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import Footer from "../component/partials/Footer";
 import Header from "../component/partials/Header";
+
 // import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <>
       <Header />
+      <main>
+        <h1>Halo</h1>
+      </main>
+      <Footer />
     </>
   );
 };
