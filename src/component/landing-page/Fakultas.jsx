1a580aff78b5d35c8d96d19b9c6e93465a6ca1bd
muhamadazis32 <muhamadazis9a@gmail.com>
2022-08-22 10:33:30 +0700

    slicing section fakultas

---
+++ src/component/landing-page/Fakultas.jsx
@@ -0,0 +1,109 @@
+import React from "react";
+
+const Fakultas = () => {
+  return (
+    <section>
+      <div className="container">
+        <div className="row mb-4">
+          <div className="col-lg-8 mx-auto text-center">
+            <h2 className="fs-1 first-color-second">
+              Fakultas <span className="title-small">Kami</span>
+            </h2>
+            <p className="mb-0">Ini adalah beberapa Fakultas kami</p>
+          </div>
+        </div>
+        <div className="tab-content" id="course-pills-tabContent">
+          <div
+            className="tab-pane fade show active"
+            id="course-pills-tabs-1"
+            role="tabpanel"
+            aria-labelledby="course-pills-tab-1"
+          >
+            <div className="row g-4">
+              <div className="col-sm-6 col-lg-4 col-xl-4">
+                <div className="card shadow h-100">
+                  <img
+                    src="assets/images/fakultas/UMKM & Ritel 1.png"
+                    className="card-img-top"
+                    alt="course image"
+                  />
+
+                  <div className="card-body pb-0">
+                    <h5 className="card-title fw-normal">
+                      <a href=" " className="first-color-second">
+                        Fakultas Bisnis & Pembiayaan
+                      </a>
+                    </h5>
+                  </div>
+
+                  <div className="card-footer pt-0 pb-3">
+                    <button
+                      type="button"
+                      className="btn bnt-sm bg-first-color text-white w-100"
+                    >
+                      Lihat Jurusan
+                    </button>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-sm-6 col-lg-4 col-xl-4">
+                <div className="card shadow h-100">
+                  <img
+                    src="assets/images/fakultas/UMKM & Ritel 2.png"
+                    className="card-img-top"
+                    alt="course image"
+                  />
+
+                  <div className="card-body pb-0">
+                    <h5 className="card-title fw-normal">
+                      <a href=" " className="first-color-second">
+                        Fakultas Desain Digital
+                      </a>
+                    </h5>
+                  </div>
+
+                  <div className="card-footer pt-0 pb-3">
+                    <button
+                      type="button"
+                      className="btn bnt-sm bg-first-color text-white w-100"
+                    >
+                      Lihat Jurusan
+                    </button>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-sm-6 col-lg-4 col-xl-4">
+                <div className="card shadow h-100">
+                  <img
+                    src="assets/images/fakultas/UMKM & Ritel 3.png"
+                    className="card-img-top"
+                    alt="course image"
+                  />
+                  <div className="card-body pb-0">
+                    <h5 className="card-title fw-normal ">
+                      <a href=" " className="first-color-second">
+                        Fakultas Teknologi pendukung ekonomi digital
+                      </a>
+                    </h5>
+                  </div>
+                  <div className="card-footer pt-0 pb-3">
+                    <button
+                      type="button"
+                      className="btn bnt-sm w-100 bg-first-color text-white "
+                    >
+                      Lihat Jurusan
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Fakultas;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Hero from "../component/landing-page/Hero";
 import Footer from "../component/partials/Footer";
 import Header from "../component/partials/Header";
 import Counter from "../component/landing-page/Counter";
+import Fakultas from "../component/landing-page/Fakultas";
 
 // import { Link } from "react-router-dom";
 
@@ -14,6 +15,7 @@ const Home = () => {
       <main>
         <Hero />
         <Counter />
+        <Fakultas />
       </main>
       <Footer />
     </>
