b55bb48be4aac75878d5e6b91c33aeb56ea94c33
rizki1912 <rizki191220@gmail.com>
2022-08-23 11:20:36 +0700

    Edit Profile

--- src/pages/EditProfile.jsx
+++ src/pages/EditProfile.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import BannerProfile from "../component/edit-profile/BannerProfile";
 import SidebarProfile from "../component/edit-profile/SidebarProfile";
-import Footer from "../component/partials/Footer";
+import FormEdit from "../component/edit-profile/FormEdit";
+import LinkedAccount from "../component/edit-profile/LinkedAccount";
+import UpdatePassword from "../component/edit-profile/UpdatePassword";
+import _Footer from "../component/partials/_Footer";
 import Header from "../component/partials/Header";
 
 const EditProfile = () => {
@@ -14,11 +17,18 @@ const EditProfile = () => {
             <div className="container">
                 <div className="row">
                     <SidebarProfile/>
+                    <div className="col-xl-9">
+                      <FormEdit />
+                      <div className="row g-4 mt-3">
+                      <LinkedAccount />
+                      <UpdatePassword />
+                      </div>
+                    </div>
                 </div>
             </div>
         </section>
       </main>
-      <Footer />
+      <_Footer />
     </>
   );
 };
