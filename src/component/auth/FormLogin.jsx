7dc0778454787b744f62eec524d230702e7fbdf8
muhamadazis32 <muhamadazis9a@gmail.com>
2022-08-31 15:00:40 +0700

    integrasi API Login

--- src/component/auth/FormLogin.jsx
+++ src/component/auth/FormLogin.jsx
@@ -1,92 +1,132 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { Navigate } from "react-router-dom";
+
+// Redux
+import { useDispatch, useSelector } from "react-redux";
+import { loginViaForm } from "../../redux/actions/AuthAction";
 
 const FormLogin = () => {
+  const dispatch = useDispatch();
+  const { isAuthenticated, isAuthenticatedLoading, user, error } = useSelector(
+    (state) => state.AuthReducer
+  );
+
+  useEffect(() => {
+    if (error) {
+      alert(error);
+    }
+  }, [error]);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    dispatch(loginViaForm({ email, password }));
+  };
+
   return (
-    <div className="col-12 col-lg-6 bg-white">
-      <div className="row my-8">
-        <div className="col-sm-10 col-xl-8 m-auto">
-          <h1 className="fs-2 mb-4">Login Kampus Gratis!</h1>
+    <>
+      {!isAuthenticated ? (
+        <div className="col-12 col-lg-6 bg-white">
+          <div className="row my-8">
+            <div className="col-sm-10 col-xl-8 m-auto">
+              <h1 className="fs-2 mb-4">Login Kampus Gratis!</h1>
 
-          <form>
-            <div className="mb-4">
-              <label htmlFor="exampleInputEmail1" className="form-label">
-                Email address *
-              </label>
-              <div className="input-group input-group-lg">
-                <span className="input-group-text bg-light rounded-start border-0 text-secondary px-3">
-                  <i className="bi bi-envelope-fill"></i>
+              <form onSubmit={handleSubmit}>
+                <div className="mb-4">
+                  <label htmlFor="exampleInputEmail1" className="form-label">
+                    Email address *
+                  </label>
+                  <div className="input-group input-group-lg">
+                    <span className="input-group-text bg-light rounded-start border-0 text-secondary px-3">
+                      <i className="bi bi-envelope-fill"></i>
+                    </span>
+                    <input
+                      type="email"
+                      className="form-control border-0 bg-light rounded-end ps-1"
+                      placeholder="E-mail"
+                      id="exampleInputEmail1"
+                      name="email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </div>
+                </div>
+
+                <div className="mb-4">
+                  <label htmlFor="inputPassword5" className="form-label">
+                    Password *
+                  </label>
+                  <div className="input-group input-group-lg">
+                    <span className="input-group-text bg-light rounded-start border-0 text-secondary px-3">
+                      <i className="fas fa-lock"></i>
+                    </span>
+                    <input
+                      type="password"
+                      className="form-control border-0 bg-light rounded-end ps-1"
+                      placeholder="password"
+                      id="inputPassword5"
+                      name="password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </div>
+                </div>
+
+                <div className="mb-4 d-flex justify-content-between mb-4">
+                  <div className="form-check"></div>
+                  <div className="text-primary-hover">
+                    <Link to={"/forgot-password"} className="text-secondary">
+                      <u>Lupa Password?</u>
+                    </Link>
+                  </div>
+                </div>
+
+                <div className="align-items-center mt-0">
+                  <div className="d-grid">
+                    <button
+                      className="btn btn-kg mb-0 text-white"
+                      type="submit"
+                    >
+                      Login
+                    </button>
+                  </div>
+                </div>
+              </form>
+
+              <div className="row">
+                <div className="position-relative my-4">
+                  <hr />
+                  <p className="small position-absolute top-50 start-50 translate-middle bg-body px-5">
+                    Atau
+                  </p>
+                </div>
+
+                <div className="col-xxl-12 d-grid">
+                  <a
+                    href=" "
+                    className="btn btn-outline-secondary mb-2 mb-xxl-0"
+                  >
+                    <i className="fab fa-fw fa-google me-2"></i>
+                    Login dengan Google
+                  </a>
+                </div>
+              </div>
+
+              <div className="mt-4 text-center">
+                <span>
+                  Tidak punya akun? <Link to="/registration"> Daftar</Link>
                 </span>
-                <input
-                  type="email"
-                  className="form-control border-0 bg-light rounded-end ps-1"
-                  placeholder="E-mail"
-                  id="exampleInputEmail1"
-                />
               </div>
             </div>
-
-            <div className="mb-4">
-              <label htmlFor="inputPassword5" className="form-label">
-                Password *
-              </label>
-              <div className="input-group input-group-lg">
-                <span className="input-group-text bg-light rounded-start border-0 text-secondary px-3">
-                  <i className="fas fa-lock"></i>
-                </span>
-                <input
-                  type="password"
-                  className="form-control border-0 bg-light rounded-end ps-1"
-                  placeholder="password"
-                  id="inputPassword5"
-                />
-              </div>
-            </div>
-
-            <div className="mb-4 d-flex justify-content-between mb-4">
-              <div className="form-check"></div>
-              <div className="text-primary-hover">
-                <a href=" " className="text-secondary">
-                  <Link to={"/forgot-password"}>
-                    <u>Lupa Password?</u>
-                  </Link>
-                </a>
-              </div>
-            </div>
-
-            <div className="align-items-center mt-0">
-              <div className="d-grid">
-                <button className="btn btn-kg mb-0 text-white" type="button">
-                  Login
-                </button>
-              </div>
-            </div>
-          </form>
-
-          <div className="row">
-            <div className="position-relative my-4">
-              <hr />
-              <p className="small position-absolute top-50 start-50 translate-middle bg-body px-5">
-                Atau
-              </p>
-            </div>
-
-            <div className="col-xxl-12 d-grid">
-              <a href=" " className="btn btn-outline-secondary mb-2 mb-xxl-0">
-                <i className="fab fa-fw fa-google me-2"></i>
-                Login dengan Google
-              </a>
-            </div>
-          </div>
-
-          <div className="mt-4 text-center">
-            <span>
-              Tidak punya akun? <Link to="/registration"> Daftar</Link>
-            </span>
           </div>
         </div>
-      </div>
-    </div>
+      ) : (
+        <Navigate to={`/`} />
+      )}
+    </>
   );
 };
 
--- src/redux/reducers/auth/index.js
+++ src/redux/reducers/auth/index.js
@@ -1,9 +1,15 @@
-import { ADD_USER } from "../../actions/AuthAction";
+import { ADD_USER, AUTH_ERROR, LOGIN, LOGOUT } from "../../actions/AuthAction";
 
 const initialState = {
   addUserResult: false,
   addUserLoading: false,
   addUserError: false,
+
+  isAuthenticated: !!localStorage.getItem("token"),
+  isAuthenticatedLoading: false,
+  accessToken: localStorage.getItem("token"),
+  user: JSON.parse(localStorage.getItem("user")),
+  error: null,
 };
 
 const AuthReducer = (state = initialState, action) => {
@@ -14,6 +20,42 @@ const AuthReducer = (state = initialState, action) => {
         addUserResult: action.payload.data,
         addUserLoading: action.payload.loading,
         addUserError: action.payload.errorMessage,
+      };
+    case LOGIN:
+      localStorage.setItem("token", action.payload.data);
+      localStorage.setItem("user", JSON.stringify(action.user));
+
+      document.querySelector("body").style.overflow = "auto";
+
+      return {
+        ...state,
+        isAuthenticated: action.payload.data,
+        isAuthenticatedLoading: action.payload.loading,
+        accessToken: action.payload.data,
+        user: action.user,
+        error: null,
+      };
+
+    case LOGOUT:
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+
+      document.querySelector("body").style.overflow = "auto";
+
+      return {
+        ...state,
+        isAuthenticated: false,
+        accessToken: null,
+        error: null,
+      };
+    case AUTH_ERROR:
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+      return {
+        ...state,
+        isAuthenticated: false,
+        accessToken: null,
+        error: action.payload.error,
       };
     default:
       return state;
