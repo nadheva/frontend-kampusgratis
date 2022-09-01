a0885216c477463c20efd6405294dc74d1585cb6
Kenniskiu Fortino Kurniawan <kenniskiufk123@gmail.com>
2022-09-01 15:25:59 +0700

    fix redux

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { createStore, compose, applyMiddleware } from "redux";
+import { legacy_createStore as createStore, compose, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import reducers from "./redux/reducers";
@@ -11,8 +11,7 @@ import reducers from "./redux/reducers";
 const store = createStore(
   reducers,
   compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(thunk)
   )
 );
 
