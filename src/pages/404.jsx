af6860482c3253ac19aae3b44226aa3c8049b1a8
muhamadazis32 <muhamadazis9a@gmail.com>
2022-09-05 10:13:10 +0700

    error handling 404

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useEffect } from "react";
 
 const PageNotFound = () => {
-  return (
-    <div>404 not found</div>
-  )
-}
+  useEffect(() => {
+    document.title = "404 | Page Not Found";
+    window.scrollTo(0, 0);
+  });
 
-export default PageNotFound
+  return (
+    <main>
+      <section className="pt-5">
+        <div className="container">
+          <div className="row">
+            <div className="col-12 text-center">
+              <img
+                src="assets/images/element/error404-01.svg"
+                className="h-200px h-md-400px mb-4"
+                alt=""
+              />
+
+              <h1 className="display-1 text-danger mb-0">404</h1>
+
+              <h2>Oopps!...Page not found</h2>
+
+              <p className="mb-4">
+                Either something went wrong or this page doesn't exist anymore.
+              </p>
+
+              <a href="/" className="btn btn-primary mb-0">
+                Go Back
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default PageNotFound;
