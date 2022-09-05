678219d5c90872119b1e75ffd421a1d294263689
muhamadazis32 <muhamadazis9a@gmail.com>
2022-09-05 11:17:01 +0700

    delete kontak

--- src/redux/reducers/index.jsx
+++ src/redux/reducers/index.jsx
@@ -1,8 +1,6 @@
 import { combineReducers } from "redux";
-import KontakReducer from "./kontak";
 import AuthReducer from "./auth";
 
 export default combineReducers({
-  KontakReducer,
   AuthReducer
 });
