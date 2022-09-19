8e6b7eed9064b7810b1b9393d0a28684600380aa
Muhamad Azis <88225954+muhamadAzis32@users.noreply.github.com>
2022-09-19 16:22:30 +0700

    Update component forum diskusi

---
+++ src/components/studiku/forumdiskusi/Intro.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Intro = () => {
+    return (
+        <section className="bg-blue py-7">
+            <div className="container">
+                <div className="row justify-content-lg-between">
+                    <div className="col-lg-8">
+                        <h1 className="text-white">Forum Diskusi Global</h1>
+                        <p className="text-white">Satisfied conveying a dependent contented he gentleman agreeable do be.
+                            Warrant private blushes removed an in equally totally if. Delivered dejection necessary
+                            objection do Mr prevailed. Mr feeling does chiefly cordial in do.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Intro
---
+++ src/components/studiku/forumdiskusi/MainContent.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import CardHeader from './CardHeader'
+import CardBody from './CardBody'
+
+const MainContent = () => {
+    return (
+        <section className="pt-0">
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <div className="card shadow rounded-2 p-0 mt-n5">
+
+                            <div className="card-body p-sm-4">
+                                <div className="tab-content" id="course-pills-tabContent">
+                                    <div className="tab-pane fade show active" id="course-pills-3" role="tabpanel"
+                                        aria-labelledby="course-pills-tab-3">
+                                        <div className="card">
+
+                                            <CardHeader />
+                                            <CardBody />
+                                 
+
+                            
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default MainContent
