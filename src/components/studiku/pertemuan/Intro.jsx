050e4fbd571fd24eb9c85bfac58637a5fd8866d5
Muhamad Azis <88225954+muhamadAzis32@users.noreply.github.com>
2022-09-25 14:58:03 +0700

    update pertemuan - menambahkan data dummy

--- src/components/studiku/pertemuan/Intro.jsx
+++ src/components/studiku/pertemuan/Intro.jsx
@@ -10,15 +10,15 @@ const Intro = () => {
                         <p className="text-white">Satisfied conveying a dependent contented he gentleman agreeable do be.
                             Warrant private blushes removed an in equally totally if. Delivered dejection necessary
                             objection do Mr prevailed. Mr feeling does chiefly cordial in do.</p>
-                        <ul class="list-inline mb-5">
-                            <li class="list-inline-item h6 me-3 mb-1 mb-sm-0 text-white">
-                                <i class="fas fa-user text-warning me-2"></i> Lori Stevens
+                        <ul className="list-inline mb-5">
+                            <li className="list-inline-item h6 me-3 mb-1 mb-sm-0 text-white">
+                                <i className="fas fa-user text-warning me-2"></i> Lori Stevens
                             </li>
-                            <li class="list-inline-item h6 me-3 mb-1 mb-sm-0 text-white">
-                                <i class="fas fa-user-graduate text-orange me-2"></i>121 Mahasiswa Enrolled
+                            <li className="list-inline-item h6 me-3 mb-1 mb-sm-0 text-white">
+                                <i className="fas fa-user-graduate text-orange me-2"></i>121 Mahasiswa Enrolled
                             </li>
-                            <li class="list-inline-item h6 me-3 mb-1 mb-sm-0 text-white">
-                                <i class="fas fa-signal text-success me-2"></i>All levels
+                            <li className="list-inline-item h6 me-3 mb-1 mb-sm-0 text-white">
+                                <i className="fas fa-signal text-success me-2"></i>All levels
                             </li>
                         </ul>
                     </div>
