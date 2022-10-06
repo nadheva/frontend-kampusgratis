45eebae6b526e36310de28f20d9e55ad735240f2
Muhamad Azis <88225954+muhamadAzis32@users.noreply.github.com>
2022-10-06 09:39:29 +0700

    Integrasi API detailModul

---
+++ src/components/studiku/detailModule/DokumentItem.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const DokumentItem = ({ dokument }) => {
+    return (
+        <>
+            <div className="d-sm-flex justify-content-sm-between align-items-center">
+                <div className="d-flex">
+                    <a href={dokument.link} download className="btn btn-light btn-round mb-0 flex-shrink-0">
+                        <i className="bi bi-file-earmark-pdf" />
+                    </a>
+                    <div className="ms-2 ms-sm-3 mt-1 mt-sm-0">
+                        <h6 className="mb-0">{dokument.description}</h6>
+                        {/* <p className="mb-2 mb-sm-0 small">Pertemuan 1</p> */}
+                    </div>
+                </div>
+            </div>
+            <hr />
+        </>
+    )
+}
+
+export default DokumentItem
