feb4baee9f67af849ca77a3065b29909307480fc
Muhamad Azis <88225954+muhamadAzis32@users.noreply.github.com>
2022-10-07 15:24:55 +0700

    Update component play vidio

--- src/pages/studiku/VidioPlayer.jsx
+++ src/pages/studiku/VidioPlayer.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect } from 'react'
 import { useParams } from "react-router-dom"
-import MainContent from '../../components/studiku/vidioPlayer/MainContent'
-import RightSidebar from '../../components/studiku/vidioPlayer/RightSidebar'
+import { toast } from 'react-toastify';
+
+// Component
+import VidioList from '../../components/studiku/vidioPlayer/VidioList';
+import Sidebar from '../../components/studiku/vidioPlayer/Sidebar';
 import Header from '../default/Header';
 import Footer from '../default/Footer';
 
@@ -10,13 +13,11 @@ import { useSelector, useDispatch } from 'react-redux'
 import { getVideos, reset } from '../../features/video/videoSlice'
 import useEffectOnce from '../../helpers/useEffectOnce';
 
-import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css';
-
 const VidioPlayer = () => {
     // Get id
     const { videoId } = useParams()
 
+    // Redux
     const dispatch = useDispatch()
     const { videos, isLoading, isError, isSuccess, message } = useSelector(
         (state) => state.video
@@ -27,20 +28,15 @@ const VidioPlayer = () => {
     });
 
     useEffect(() => {
-
         if (isError && !isSuccess) {
-            // toast.error(message);
+            toast.error(message);
             dispatch(reset());
         }
-
         if (isSuccess && message && !isError) {
-            // toast.success(message);
+            toast.success(message);
             dispatch(reset());
         }
-
     }, [videos, isLoading, isError, isSuccess, message, dispatch])
-
-    console.log(videos)
 
     return (
         <>
@@ -49,32 +45,11 @@ const VidioPlayer = () => {
                 <section className="pt-3 pt-xl-5">
                     <div className="container" data-sticky-container>
                         <div className="row g-4">
-                            <div className="col-xl-8">
-                                <div className="row g-4">
-                                    {
-                                        videos == null || videos == 0 ? (
-                                            <h2>Data kosong</h2>
-                                        ) : isLoading ? (
-                                            <div className='row'>
-                                                <div className="col-sm-12 col-xl-12">
-                                                    <SkeletonTheme>
-                                                        <Skeleton height={300} />
-                                                    </SkeletonTheme>
-                                                </div>
-                                                <div className="col-sm-12 col-xl-12">
-                                                    <SkeletonTheme>
-                                                        <Skeleton height={100} />
-                                                    </SkeletonTheme>
-                                                </div>
-                                            </div>
-                                        ) : (
-                                            <MainContent videos={videos} />
-                                        )
-                                    }
-
-                                </div>
-                            </div>
-                            <RightSidebar />
+                            <VidioList
+                                videos={videos}
+                                isLoading={isLoading}
+                            />
+                            <Sidebar />
                         </div>
                     </div>
                 </section>
