007d102e6c50203d9082523cf922e7024db38348
Muhamad Azis <88225954+muhamadAzis32@users.noreply.github.com>
2022-10-11 18:49:18 +0700

    fix bug img matakuliah

--- src/components/studiku/homeStudiku/CourseItem.jsx
+++ src/components/studiku/homeStudiku/CourseItem.jsx
@@ -2,25 +2,33 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 function CourseItem({ course }) {
+  // console.log(course.item.Subject.thumbnail_link)
   return (
     <div className="col-sm-6 col-xl-4">
-      <div className="card shadow  h-100">
-        <img src={course.item.Subject.thumbnail_link} className="card-img-top" alt="courseimage" />
+      <div className="card shadow  h-100 ">
+        <img src={course.item.Subject.thumbnail_link} className="card-img-top card-img-artikel" alt="courseimage" />
         <div className="card-body pb-0">
           <div className="d-flex justify-content-between mb-2">
-            <a href=" " className="badge bg-success bg-opacity-10 text-success"> {course.lecturers} </a>
-            <a href=" " className="badge bg-purple bg-opacity-10 text-purple">{course.progress}%</a>
+            <p className="badge bg-success bg-opacity-10 text-success"> {course.item.Subject.level}</p>
+            <p className="badge bg-purple bg-opacity-10 text-purple">{course.progress}%</p>
           </div>
           <h5 className="card-title">
             <Link to={`/pertemuan/${course.item.id}`}>{course.item.Subject.name}</Link>
           </h5>
-          <p className="mb-2 text-truncate-2">Jumlah Mahasiswa {course.student_count}</p>
+          <p className="mb-2 text-truncate-2">
+            <b>Dosen : </b>
+            {course.lecturers}
+          </p>
         </div>
         <div className="card-footer pt-0 pb-3">
           <hr />
           <div className="d-flex justify-content-between">
-            <span className="h6 fw-light mb-0"><i className="fas fa-table text-danger me-2"></i>{course.item.Subject.credit} Sks</span>
-            <span className="h6 fw-light mb-0"><i className="fas fa-signal text-success me-2"></i>{course.item.Subject.level}</span>
+            <span className="h6 fw-light mb-0">
+              <i className="fas fa-table text-danger me-2"></i>{course.item.Subject.credit} Sks
+            </span>
+            <span className="h6 fw-light mb-0">
+              <i className="fas fa-user text-success me-2"></i>{course.student_count} Mahasiswa
+            </span>
           </div>
         </div>
       </div>
