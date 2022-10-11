181ea00b16c9cb17f93d8bcd2d10810a8da95d3c
Muhamad Azis <88225954+muhamadAzis32@users.noreply.github.com>
2022-10-11 12:34:00 +0700

    update

--- src/index.js
+++ src/index.js
@@ -13,17 +13,17 @@ root.render(
 		<App />
 		<Helmet>
 			<script
-				type="text/babel"
+				type="text/javascript"
 				src="/assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"
 				defer
 			></script>
 			<script
-				type="text/babel"
+				type="text/javascript"
 				src="/assets/vendor/choices/js/choices.min.js"
 				defer
 			></script>
 			<script
-				type="text/babel"
+				type="text/javascript"
 				src="/assets/js/functions.js"
 				defer
 			></script>
