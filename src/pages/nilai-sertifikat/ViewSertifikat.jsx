6c4ba41071f0255d13a0e2529ccd2ed1c792c2f5
Muhamad Azis <88225954+muhamadAzis32@users.noreply.github.com>
2022-10-28 13:21:51 +0700

    integrasi API detail sertifikat

--- src/pages/nilai-sertifikat/ViewSertifikat.jsx
+++ src/pages/nilai-sertifikat/ViewSertifikat.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
-// import { useParams } from 'react-router-dom';
+import React, { useState } from 'react'
+import { useParams } from 'react-router-dom';
 
+// Component
 import Header from "../default/Header";
 import Footer from "../default/Footer";
 
+// redux
+import { useSelector, useDispatch } from "react-redux";
+import { getCertificate, reset, resetAll } from "../../features/sertifikat/certificateSlice";
+import useEffectOnce from "../../helpers/useEffectOnce";
+import { useEffect } from 'react';
+
 const ViewSertifikat = () => {
-    // let id = useParams();
+    let { id } = useParams();
+
+    // Redux
+    const dispatch = useDispatch();
+    const [currentCertificate, setCurrentCertificate] = useState({});
+
+    const { data, isLoading } = useSelector(
+        (state) => state.certificate
+    );
+
+    useEffectOnce(() => {
+        dispatch(getCertificate(id));
+    });
+
+    useEffect(() => {
+        if (data?.certificate) setCurrentCertificate(data.certificate);
+
+    }, [data]);
+    // console.log(currentCertificate.link)
+    // console.log(currentCertificate.thumbnail_link)
     return (
         <>
             <Header />
@@ -17,7 +43,7 @@ const ViewSertifikat = () => {
                                 <div
                                     className="card card-body img-object shadow p-2 h-300px h-sm-400px h-md-500px h-lg-500px  position-relative overflow-hidden"
                                     style={{
-                                        backgroundImage: "url(/assets/images/about/29.jpeg)",
+                                        backgroundImage: `url(${currentCertificate.thumbnail_link})`,
                                         backgroundPosition: "center left",
                                         backgroundSize: "cover"
                                     }}
@@ -26,7 +52,7 @@ const ViewSertifikat = () => {
                                     <div className="card-img-overlay">
                                         <div className="position-absolute top-50 start-50 translate-middle">
                                             <a
-                                                href="https://firebasestorage.googleapis.com/v0/b/kampus-gratis2.appspot.com/o/documents%2Fcertificate%2FK739FJY2G5LQ-aryo%20bhodro-certificat.pdf?alt=media&token=489f09ab-5454-483f-8efb-ceb06eb34e68z"
+                                                href={currentCertificate.link}
                                                 className="btn btn-lg text-info btn-round btn-white-shadow mb-0"
                                                 data-glightbox=""
                                                 data-gallery="video-tour"
@@ -41,8 +67,8 @@ const ViewSertifikat = () => {
                                 <div className="col-auto my-2 d-none d-lg-block pt-4">
                                     <a
                                         className="btn btn-info"
-                                        href="assets/images/about/29.jpeg"
-                                        download=""
+                                        href=" "
+                                        download
                                     >
                                         <i className="fas fa-download me-2" />
                                         Download Sertifikat
