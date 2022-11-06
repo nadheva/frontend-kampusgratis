54afa17150864acc3e4bc2951a1a20c86861f4be
Muhamad Azis <88225954+muhamadAzis32@users.noreply.github.com>
2022-11-06 21:58:31 +0700

    update component update profile

--- src/components/Account/Form.jsx
+++ src/components/Account/Form.jsx
@@ -1,28 +1,24 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
-import { useNavigate } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import {
-	updateProfile,
-	getMe,
-	resetState,
-} from "../../features/profile/profileSlice";
-import useEffectOnce from "../../helpers/useEffectOnce";
-import { toast } from "react-toastify";
+import { useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { updateProfile, getMe, resetState } from '../../features/profile/profileSlice';
+import useEffectOnce from '../../helpers/useEffectOnce';
+import { toast } from 'react-toastify';
 
 const FormEdit = () => {
 	const genderOptions = [
-		{ value: "0", label: "Tidak ingin memberi tahu" },
-		{ value: "1", label: "Laki-laki" },
-		{ value: "2", label: "Perempuan" },
+		{ value: '0', label: 'Tidak ingin memberi tahu' },
+		{ value: '1', label: 'Laki-laki' },
+		{ value: '2', label: 'Perempuan' }
 	];
 
 	const [isLoaded, setIsLoaded] = useState(false);
 	const [profileData, setProfileData] = useState({
-		full_name: "",
+		full_name: '',
 		gender: 0,
-		phone: "",
-		display_picture: "",
+		phone: '',
+		display_picture: ''
 	});
 
 	const [imageData, setImageData] = useState(null);
@@ -30,8 +26,7 @@ const FormEdit = () => {
 	const navigate = useNavigate();
 	const dispatch = useDispatch();
 
-	const { full_name, gender, phone, username, display_picture_link } =
-		profileData; // display_picture,
+	const { full_name, gender, phone, username, display_picture_link } = profileData; // display_picture, 
 
 	const { user, isLoading, isError, isSuccess, message } = useSelector(
 		(state) => state.profile
@@ -46,7 +41,7 @@ const FormEdit = () => {
 		if (e.target.files[0]) {
 			const reader = new FileReader();
 
-			reader.addEventListener("load", () => {
+			reader.addEventListener('load', () => {
 				setImageData(reader.result);
 			});
 
@@ -54,36 +49,35 @@ const FormEdit = () => {
 
 			setProfileData((prevState) => ({
 				...prevState,
-				[e.target.name]: e.target.files[0],
+				[e.target.name]: e.target.files[0]
 			}));
 		}
-	};
+	}
 
 	const onFormChange = (e) => {
 		setProfileData((prevState) => ({
 			...prevState,
-			[e.target.name]: e.target.value,
+			[e.target.name]: e.target.value
 		}));
-	};
+	}
 
 	const onFormSubmit = (e) => {
 		e.preventDefault();
 		setIsLoaded(false);
 		dispatch(updateProfile(profileData));
-	};
+	}
 
 	useEffect(() => {
 		setIsLoaded(true);
 
-		if (user)
-			setProfileData({
-				full_name: user.full_name,
-				phone: user.phone,
-				gender: user.gender,
-				username: user.username,
-				display_picture: user.display_picture,
-				display_picture_link: user.display_picture_link,
-			});
+		if (user) setProfileData({
+			full_name: user.full_name,
+			phone: user.phone,
+			gender: user.gender,
+			username: user.username,
+			display_picture: user.display_picture,
+			display_picture_link: user.display_picture_link
+		});
 
 		if (isError && !isSuccess) {
 			toast.error(message);
@@ -96,99 +90,75 @@ const FormEdit = () => {
 		}
 	}, [isError, isSuccess, user, message, navigate, dispatch]);
 
-	return (
-		<>
-			{isLoading ? (
-				<>
-					<div
-						className="container text-center"
-						style={{ marginTop: "188px", marginBottom: "188px" }}
-					>
-						<div className="row">
-							<div className="col-12">
-								<div className="spinner-border" role="status">
-									<span className="visually-hidden">Loading...</span>
-								</div>
-							</div>
+	return <>
+		{isLoading ? <>
+			<div className='container text-center' style={{ marginTop: '188px', marginBottom: '188px' }}>
+				<div className='row'>
+					<div className='col-12'>
+						<div className='spinner-border' role='status'>
+							<span className='visually-hidden'>Loading...</span>
 						</div>
 					</div>
-				</>
-			) : (
-				<>
-					{user ? (
-						<>
-							<div className="card border rounded-3">
-								<div className="card-header border-bottom">
-									<h3 className="card-header-title mb-0">Profil</h3>
+				</div>
+			</div>
+		</> : <>
+			{user ? <>
+				<div className='card border rounded-3'>
+					<div className='card-header border-bottom'>
+						<h3 className='card-header-title mb-0'>Profil</h3>
+					</div>
+					<div className='card-body'>
+						<form className='row g-4' onSubmit={onFormSubmit}>
+							<div className='col-12 justify-content-center align-items-center'>
+								<label className='form-label'>Tampilan Foto</label>
+								<div className='d-flex align-items-center'>
+									<label
+										className='position-relative me-4'
+										htmlFor='uploadfile-1'
+										title='Replace this pic'
+									>
+										<span className='avatar avatar-xl'>
+											<img
+												id='uploadfile-1-preview'
+												className='avatar-img rounded-circle border border-white border-3 shadow'
+												src={imageData ? imageData : display_picture_link != null ? display_picture_link : 'assets/images/avatar/empty-display-picture.png'}
+												alt=''
+											/>
+										</span>
+									</label>
+									<label className='btn btn-primary-soft mb-0' htmlFor='uploadfile-1'>
+										Ubah
+									</label>
+									<input
+										id='uploadfile-1'
+										name='display_picture'
+										className='form-control d-none'
+										type='file'
+										onChange={onChangePicture}
+									/>
 								</div>
-								<div className="card-body">
-									<form className="row g-4" onSubmit={onFormSubmit}>
-										<div className="col-12 justify-content-center align-items-center">
-											<label className="form-label">Tampilan Foto</label>
-											<div className="d-flex align-items-center">
-												<label
-													className="position-relative me-4"
-													htmlFor="uploadfile-1"
-													title="Replace this pic"
-												>
-													<span className="avatar avatar-xl">
-														<img
-															id="uploadfile-1-preview"
-															className="avatar-img rounded-circle border border-white border-3 shadow"
-															src={
-																imageData
-																	? imageData
-																	: display_picture_link != null
-																	? display_picture_link
-																	: "assets/images/avatar/empty-display-picture.png"
-															}
-															alt=""
-														/>
-													</span>
-												</label>
-												<label
-													className="btn btn-primary-soft mb-0"
-													htmlFor="uploadfile-1"
-												>
-													Ubah
-												</label>
-												<input
-													id="uploadfile-1"
-													name="display_picture"
-													className="form-control d-none"
-													type="file"
-													onChange={onChangePicture}
-												/>
-											</div>
-										</div>
-										<div className="col-6">
-											<label className="form-label">Nama Lengkap</label>
-											<div className="input-group">
-												<input
-													type="text"
-													className="form-control"
-													name="full_name"
-													value={full_name}
-													onChange={onFormChange}
-													placeholder="Nama Lengkap"
-												/>
-											</div>
-										</div>
-										<div className="col-md-6">
-											<label className="form-label">Username</label>
-											<div className="input-group">
-												<span className="input-group-text">
-													kampusgratis.com
-												</span>
-												<input
-													type="text"
-													className="form-control"
-													value={username}
-													readOnly
-												/>
-											</div>
-										</div>
-										{/* <div className='col-md-6'>
+							</div>
+							<div className='col-6'>
+								<label className='form-label'>Nama Lengkap</label>
+								<div className='input-group'>
+									<input
+										type='text'
+										className='form-control'
+										name='full_name'
+										value={full_name}
+										onChange={onFormChange}
+										placeholder='Nama Lengkap'
+									/>
+								</div>
+							</div>
+							<div className='col-md-6'>
+								<label className='form-label'>Username</label>
+								<div className='input-group'>
+									<span className='input-group-text'>kampusgratis.com</span>
+									<input type='text' className='form-control' value={username} readOnly />
+								</div>
+							</div>
+							{/* <div className='col-md-6'>
             <label className='form-label'>Alamat Email</label>
             <input
               className='form-control'
@@ -198,59 +168,43 @@ const FormEdit = () => {
               readOnly
             />
           </div> */}
-										<div className="col-md-6">
-											<label className="form-label">Nomor Handphone</label>
-											<input
-												type="text"
-												className="form-control"
-												name="phone"
-												value={phone}
-												onChange={onFormChange}
-												placeholder="Nomor Handphone"
-											/>
-										</div>
-										<div className="col-md-6">
-											<label className="form-label">Jenis Kelamin</label>
-											<select
-												className="form-select"
-												onChange={onFormChange}
-												name="gender"
-												value={gender}
-											>
-												{genderOptions.map((option) => (
-													<option key={option.value} value={option.value}>
-														{option.label}
-													</option>
-												))}
-											</select>
-										</div>
-										<div className="d-sm-flex justify-content-end">
-											{isLoading && !isLoaded ? (
-												<button
-													type="submit"
-													className="btn btn-primary mb-0"
-													disabled={isLoading}
-												>
-													<span className="spinner-border spinner-border-sm"></span>
-													&nbsp; Menyimpan Perubahan ...
-												</button>
-											) : (
-												<button type="submit" className="btn btn-primary mb-0">
-													Simpan Perubahan
-												</button>
-											)}
-										</div>
-									</form>
-								</div>
+							<div className='col-md-6'>
+								<label className='form-label'>Nomor Handphone</label>
+								<input
+									type='text'
+									className='form-control'
+									name='phone'
+									value={phone}
+									onChange={onFormChange}
+									placeholder='Nomor Handphone'
+								/>
 							</div>
-						</>
-					) : (
-						<></>
-					)}
-				</>
-			)}
-		</>
-	);
+							<div className='col-md-6'>
+								<label className='form-label'>Jenis Kelamin</label>
+								<select className='form-select' onChange={onFormChange} name='gender' value={gender}>
+									{genderOptions.map(option => (
+										<option key={option.value} value={option.value} >{option.label}</option>
+									))}
+								</select>
+							</div>
+							<div className='d-sm-flex justify-content-end'>
+								{isLoading && !isLoaded ? (
+									<button type='submit' className='btn btn-primary mb-0' disabled={isLoading}>
+										<span className='spinner-border spinner-border-sm'></span>&nbsp;
+										Menyimpan Perubahan ...
+									</button>
+								) : (
+									<button type='submit' className='btn btn-primary mb-0'>
+										Simpan Perubahan
+									</button>
+								)}
+							</div>
+						</form>
+					</div>
+				</div>
+			</> : <></>}
+		</>}
+	</>
 };
 
 export default FormEdit;
