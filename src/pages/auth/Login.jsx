1ea9500a0ac451d10e0cc55f8aab2d44c4330693
Muhamad Azis <88225954+muhamadAzis32@users.noreply.github.com>
2022-11-07 14:35:09 +0700

    Update ui login

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,24 +1,15 @@
-import React, { useState, useEffect } from "react";
-import { useNavigate, Link } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import {
-	login,
-	loginWithGoogle,
-	reset,
-	resetDefault,
-} from "../../features/auth/authSlice";
-import { toast } from "react-toastify";
-import useEffectOnce from "../../helpers/useEffectOnce";
-import { getMe } from "../../features/profile/profileSlice";
+import useEffectOnce from '../../helpers/useEffectOnce';
+
+import React, { useState, useEffect } from 'react';
+import { login, loginWithGoogle, reset, resetDefault } from '../../features/auth/authSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate, Link } from 'react-router-dom';
+import { getMe } from '../../features/profile/profileSlice';
+import { toast } from 'react-toastify';
 
 function Login() {
-	useEffect(() => {
-		document.title = "Kampus Gratis | Login";
-	});
-
 	const [loginData, setLoginData] = useState({
-		email: "",
-		password: "",
+		email: '', password: ''
 	});
 
 	const { email, password } = loginData;
@@ -30,11 +21,13 @@ function Login() {
 		(state) => state.auth
 	);
 
-	const { user } = useSelector((state) => state.profile);
+	const { user } = useSelector(
+		(state) => state.profile
+	);
 
 	useEffectOnce(() => {
 		if (user && token) {
-			return navigate("/");
+			return navigate('/');
 		}
 
 		dispatch(reset());
@@ -51,139 +44,78 @@ function Login() {
 		}
 
 		if (isSuccess && token && user) {
-			navigate("/");
-			toast.success(
-				`Selamat datang di Kampus Gratis, ${user.full_name.split(" ")[0]}!`
-			);
+			navigate('/');
+			toast.success(`Selamat datang di Kampus Gratis, ${user.full_name.split(" ")[0]}!`);
 			dispatch(resetDefault());
 		}
-	}, [isError, isSuccess, user, message, navigate, dispatch]);
+
+	}, [isError, isSuccess, token, user, message, navigate, dispatch]);
 
 	const onFormChange = (e) => {
 		setLoginData((prevState) => ({
 			...prevState,
-			[e.target.name]: e.target.value,
+			[e.target.name]: e.target.value
 		}));
-	};
+	}
 
 	const onFormSubmit = (e) => {
 		e.preventDefault();
 
-		if (!email || !password)
-			return toast.error("Invalid combination Email address and Password.");
+		if (!email || !password) return toast.error("Invalid combination Email address and Password.");
 
 		const userData = {
 			email,
-			password,
+			password
 		};
 
 		dispatch(login(userData));
-	};
+	}
 
 	const onLoginWithGoogle = (e) => {
 		e.preventDefault();
 		dispatch(loginWithGoogle());
-	};
+	}
 
 	return (
 		<>
 			<main>
-				<section className="p-0 d-flex align-items-center position-relative overflow-hidden">
+				<section className="p-0 d-flex align-items-center position-relative overflow-hidden" >
 					<div className="container-fluid">
-						<div className="row bg-custom">
-							<div className="col-12 col-lg-6 d-md-flex align-items-center justify-content-center bg-opacity-10 vh-lg-100 bg-custom bg-line">
-								<div className="p-3 p-lg-5 bg-blur mx-4">
-									<div className="text-left">
-										<h1 className="fw-bold text-white">
-											Selamat Datang <br /> Di Kampus Gratis!
-										</h1>
-										<p className="mb-0 h6 fw-light text-white">
-											Dapatkan materi yang kamu butuhkan dari pakar sekarang!
-										</p>
+						<div className="row">
+							<div className="col-12 col-lg-6 d-md-flex align-items-center justify-content-center bg-primary bg-opacity-10">
+								<div className="p-3 p-lg-5">
+									<div className="text-center">
+										<h2 className="fw-bold">Selamat datang di Kampus Gratis</h2>
+										<p className="mb-0 h6 fw-light">Dapatkan materi yang kamu butuhkan dari pakar sekarang!</p>
 									</div>
-									<img
-										src="/assets/images/element/02.svg"
-										className="mt-5"
-										alt=""
-									/>
+									<img src="assets/images/element/02.svg" className="mt-5" alt="" />
 								</div>
 							</div>
-							<div className="col-12 col-lg-6 bg-white pt-3">
+							<div className="col-12 col-lg-6 m-auto">
 								<div className="row my-5">
 									<div className="col-sm-10 col-xl-8 m-auto">
-										<div className="text-center">
-											<Link className="me-0" to="/">
-												<img
-													className="light-mode-item h-40px"
-													src="/assets/images/logo-kampus-gratis.png"
-													alt="Kampus Gratis"
-												/>
-												<img
-													className="dark-mode-item h-40px"
-													src="/assets/images/logo-kampus-gratis.png"
-													alt="Kampus Gratis"
-												/>
-											</Link>
-										</div>
-										<h1 className="fs-2 pt-5">Masuk ke Kampus Gratis!</h1>
-										<p className="lead mb-4">
-											Halooo! Login dengan Akun yang kamu daftarkan.
-										</p>
+										<span className="mb-0 fs-1">👋</span>
+										<h1 className="fs-2">Masuk ke Kampus Gratis!</h1>
+										<p className="lead mb-4">Halooo! Login dengan Akun yang kamu daftarkan.</p>
 										<form onSubmit={onFormSubmit}>
 											<div className="mb-4">
-												<label htmlFor="input-email" className="form-label">
-													Alamat Email *
-												</label>
+												<label htmlFor="input-email" className="form-label">Alamat Email *</label>
 												<div className="input-group input-group-lg">
-													<span className="input-group-text bg-light rounded-start border-0 text-secondary px-3">
-														<i className="bi bi-envelope-fill"></i>
-													</span>
-													<input
-														name="email"
-														type="text"
-														className="form-control border-0 bg-light rounded-end ps-1"
-														placeholder="Alamat Email"
-														id="input-email"
-														value={email}
-														onChange={onFormChange}
-													/>
+													<span className="input-group-text bg-light rounded-start border-0 text-secondary px-3"><i className="bi bi-envelope-fill"></i></span>
+													<input name="email" type="text" className="form-control border-0 bg-light rounded-end ps-1" placeholder="Alamat Email" id="input-email" value={email} onChange={onFormChange} />
 												</div>
 											</div>
 											<div className="mb-4">
-												<label htmlFor="input-password" className="form-label">
-													Password *
-												</label>
+												<label htmlFor="input-password" className="form-label">Password *</label>
 												<div className="input-group input-group-lg">
-													<span className="input-group-text bg-light rounded-start border-0 text-secondary px-3">
-														<i className="fas fa-lock"></i>
-													</span>
-													<input
-														name="password"
-														type="password"
-														className="form-control border-0 bg-light rounded-end ps-1"
-														placeholder="Password"
-														id="input-password"
-														value={password}
-														onChange={onFormChange}
-													/>
+													<span className="input-group-text bg-light rounded-start border-0 text-secondary px-3"><i className="fas fa-lock"></i></span>
+													<input name="password" type="password" className="form-control border-0 bg-light rounded-end ps-1" placeholder="Password" id="input-password" value={password} onChange={onFormChange} />
 												</div>
-												{/* <div id="passwordHelpBlock" className="form-text">
-							Your password must be 8 characters at least
-						  </div> */}
 											</div>
 											<div className="mb-4 d-flex justify-content-between mb-4">
 												<div className="form-check">
-													<input
-														type="checkbox"
-														className="form-check-input"
-														id="exampleCheck1"
-													/>
-													<label
-														className="form-check-label"
-														htmlFor="exampleCheck1"
-													>
-														Ingat saya
-													</label>
+													<input type="checkbox" className="form-check-input" id="exampleCheck1" />
+													<label className="form-check-label" htmlFor="exampleCheck1">Ingat saya</label>
 												</div>
 												<div className="text-primary-hover">
 													<Link to="/forgot-password" className="text-secondary">
@@ -207,28 +139,18 @@ function Login() {
 										<div className="row">
 											<div className="position-relative my-4">
 												<hr />
-												<p className="small position-absolute top-50 start-50 translate-middle bg-body px-5">
-													Atau
-												</p>
+												<p className="small position-absolute top-50 start-50 translate-middle bg-body px-5">Atau</p>
 											</div>
 										</div>
 										<div className="align-items-center mt-0">
 											<div className="d-grid">
 												{isLoading ? (
-													<button
-														className="btn bg-google mb-2 mb-xxl-0"
-														disabled={isLoading}
-													>
-														<span className="spinner-border spinner-border-sm"></span>
-														&nbsp;
-														<i className="fab fa-fw fa-google text-white me-2"></i>
+													<button className="btn bg-google mb-2 mb-xxl-0" disabled={isLoading}>
+														<span className="spinner-border spinner-border-sm"></span>&nbsp;
 														&nbsp;Loading...
 													</button>
 												) : (
-													<button
-														className="btn bg-google mb-2 mb-xxl-0"
-														onClick={onLoginWithGoogle}
-													>
+													<button className="btn bg-google mb-2 mb-xxl-0" onClick={onLoginWithGoogle}>
 														<i className="fab fa-fw fa-google text-white me-2"></i>
 														Masuk dengan Google
 													</button>
@@ -236,10 +158,7 @@ function Login() {
 											</div>
 										</div>
 										<div className="mt-4 text-center">
-											<span>
-												Belum punya akun?{" "}
-												<Link to="/register">Daftar di sini</Link>
-											</span>
+											<span>Belum punya akun? <Link to="/register">Daftar di sini</Link></span>
 										</div>
 									</div>
 								</div>
@@ -249,7 +168,7 @@ function Login() {
 				</section>
 			</main>
 		</>
-	);
+	)
 }
 
 export default Login;
