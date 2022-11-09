f55fadeee6848a6348fddef9f3a24e702b8b13e4
Muhamad Azis <88225954+muhamadAzis32@users.noreply.github.com>
2022-11-09 09:36:51 +0700

    Fix search di studiku

--- src/components/My-Study/SubjectsList.jsx
+++ src/components/My-Study/SubjectsList.jsx
@@ -70,11 +70,11 @@ const SubjectsList = () => {
 	return <>
 		<div className="col-lg-12">
 			<div className="row mb-4 align-items-center">
-				<div className="col-lg-8">
-					<form className="border rounded p-2" onSubmit={doFilter}>
+				<div className="col-lg-12 ">
+					<form className="rounded p-2 shadow" onSubmit={doFilter}>
 						<div className="input-group input-borderless">
 							<input className="form-control me-1" type="search" placeholder="Cari Mata Kuliah" onChange={e => setSearchTerm(e.target.value)} />
-							<button type="button" className="btn btn-primary mb-0 rounded z-index-1"><i className="fas fa-search"></i></button>
+							<button type="submit" className="btn btn-primary mb-0 rounded z-index-1"><i className="fas fa-search"></i></button>
 						</div>
 					</form>
 				</div>
