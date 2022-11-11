1537f141bf782f7b6cbce6939c672f7d599026d0
Muhamad Azis <88225954+muhamadAzis32@users.noreply.github.com>
2022-11-11 14:13:08 +0700

    update penugasan

--- src/components/My-Study/penugasan/main-penugasan/ItemSemua.jsx
+++ src/components/My-Study/penugasan/main-penugasan/ItemSemua.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 
-const ItemSemua = () => {
+const ItemSemua = ({ assignment }) => {
+
     return (
-        <div>ItemSemua</div>
+        <div
+            className="card border-start border-success border-4 shadow-lg rounded-2 px-4 py-2 my-4">
+            <div className="d-flex justify-content-between align-items-center">
+                <div className="mt-3">
+                    <h5 className="m-0 mb-1">
+                        {assignment?.Subject?.name}
+                    </h5>
+                    <p>
+                        {assignment?.Subject?.name}
+                    </p>
+                </div>
+                <div className="d-flex flex-column align-items-center">
+                    <i className="bi bi-clock-history fs-5 text-success"></i>
+                    <p className="m-0 text-success small">{assignment?.status}</p>
+                    <p className="m-0 small">{assignment?.deadline}</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/components/My-Study/penugasan/main-penugasan/ListSemua.jsx
+++ src/components/My-Study/penugasan/main-penugasan/ListSemua.jsx
@@ -8,13 +8,13 @@ const ListSemua = ({ assignments, isLoading }) => {
     const [allAsignments, setAllAsignments] = useState([]);
 
     useEffect(() => {
-        if(allAsignments.length === 0 && Object.values(assignments).length !== 0) {
+        if (allAsignments.length === 0 && Object.values(assignments).length !== 0) {
             assignments?.ongoing.map(ongoing => setAllAsignments(prevValue => [...prevValue, ongoing]));
             assignments?.grading.map(grading => setAllAsignments(prevValue => [...prevValue, grading]));
             assignments?.finished.map(finished => setAllAsignments(prevValue => [...prevValue, finished]));
             assignments?.late.map(late => setAllAsignments(prevValue => [...prevValue, late]));
         }
-    }, [allAsignments,assignments]);
+    }, [allAsignments, assignments]);
 
     return (
         <div className="tab-pane fade show active"
@@ -32,25 +32,30 @@ const ListSemua = ({ assignments, isLoading }) => {
                     {
                         allAsignments.length !== 0 ? <>
                             {
-                                allAsignments.map(assignment => <>
-                                    <div className="card border-start border-success border-4 shadow-lg rounded-2 px-4 py-2 my-4">
-                                        <div className="d-flex justify-content-between align-items-center">
-                                            <div className="mt-3">
-                                                <h5 className="m-0 mb-1">
-                                                    {assignment.Subject.name}
-                                                </h5>
-                                                <p>
-                                                    Indonesia
-                                                </p>
-                                            </div>
-                                            <div className="d-flex flex-column align-items-center">
-                                                <i className="bi bi-clock-history fs-5 text-success"></i>
-                                                <p className="m-0 text-success small">{assignment.status}</p>
-                                                <p className="m-0 small">Deadline 24 Juni 2022, 09:00</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </>)
+
+                                allAsignments.map((assignment) => (
+                                    <ItemSemua key={assignment.id} assignment={assignment} />
+                                ))
+
+                                // allAsignments.map(assignment => <>
+                                //     <div className="card border-start border-success border-4 shadow-lg rounded-2 px-4 py-2 my-4">
+                                //         <div className="d-flex justify-content-between align-items-center">
+                                //             <div className="mt-3">
+                                //                 <h5 className="m-0 mb-1">
+                                //                     {assignment.Subject.name}
+                                //                 </h5>
+                                //                 <p>
+                                //                     Indonesia
+                                //                 </p>
+                                //             </div>
+                                //             <div className="d-flex flex-column align-items-center">
+                                //                 <i className="bi bi-clock-history fs-5 text-success"></i>
+                                //                 <p className="m-0 text-success small">{assignment.status}</p>
+                                //                 <p className="m-0 small">Deadline 24 Juni 2022, 09:00</p>
+                                //             </div>
+                                //         </div>
+                                //     </div>
+                                // </>)
                             }
                         </> : <>
                             <div className="card border border-info border-2 shadow-lg rounded-2 px-4 py-2 my-4">
@@ -65,8 +70,8 @@ const ListSemua = ({ assignments, isLoading }) => {
                 </>
             }
 
-            {/* Ongoing */}
-            <div className="card border-start border-primary border-4 shadow-lg rounded-2 px-4 py-2 my-4">
+          
+            {/* <div className="card border-start border-primary border-4 shadow-lg rounded-2 px-4 py-2 my-4">
                 <div className="d-flex justify-content-between align-items-center">
                     <div className="mt-3">
                         <h5 className="m-0 mb-1">
@@ -84,7 +89,7 @@ const ListSemua = ({ assignments, isLoading }) => {
                 </div>
             </div>
 
-            {/* Terlambat */}
+           
             <div className="card border-start border-danger border-4 shadow-lg rounded-2 px-4 py-2 my-4">
                 <div className="d-flex justify-content-between align-items-center">
                     <div className="mt-3">
@@ -103,7 +108,7 @@ const ListSemua = ({ assignments, isLoading }) => {
                 </div>
             </div>
 
-            {/* Selesai */}
+       
             <div className="card border-start border-success border-4 shadow-lg rounded-2 px-4 py-2 my-4">
                 <div className="d-flex justify-content-between align-items-center">
                     <div className="mt-3">
@@ -120,7 +125,7 @@ const ListSemua = ({ assignments, isLoading }) => {
                         <p className="m-0 small">Deadline 24 Juni 2022, 09:00</p>
                     </div>
                 </div>
-            </div>
+            </div> */}
 
         </div>
     )
