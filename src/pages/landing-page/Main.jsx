d980552b7becf016957bc15263bb0d935facf109
Muhamad Azis <88225954+muhamadAzis32@users.noreply.github.com>
2022-11-26 13:53:18 +0700

    fix bug notifikasi

--- src/pages/landing-page/Main.jsx
+++ src/pages/landing-page/Main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -16,6 +16,11 @@ import ClientSTART from "../../components/landing-page/ClientSTART";
 import WhayUseSection from "../../components/landing-page/WhayUseSection";
 
 const Main = () => {
+
+	useEffect(() => {
+		document.title = "Kampus Gratis";
+	});
+
 	return (
 		<>
 			<Header />
