72eff90db40dfced9c6af3e30607a8f30cf00cf9
ary-bod <ibhodro123@gmail.com>
2022-12-05 11:43:32 +0700

    refix display image at register & login

--- src/components/auth/VektorImage.jsx
+++ src/components/auth/VektorImage.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const VektorImage = () => {
     return (
-        <div className="col-lg-6 d-flex align-items-center order-2 order-lg-1">
+        <div className="col-lg-6 d-flex align-items-center d-none d-lg-block">
             <div className="p-3 p-lg-5">
-                <img src="assets/images/element/signin.svg" alt="" />
+                <img src="assets/images/element/signin.svg" alt="Register / Masuk Kampus Gratis" />
             </div>
             <div className="vr opacity-1 d-none d-lg-block" />
         </div>
