5b961385ea6c6a7b3bba655aede9d2ed70e3f64b
Muhamad Azis <88225954+muhamadAzis32@users.noreply.github.com>
2022-12-14 19:51:58 +0700

    Update bug

--- src/pages/panduan/VidioDetail.jsx
+++ src/pages/panduan/VidioDetail.jsx
@@ -39,72 +39,63 @@ const VidioDetail = () => {
         <>
             <Header />
             <main>
-                <section className="pt-3 pt-xl-5">
-                    <div className="container" data-sticky-container>
-                        <div className="row g-4 justify-content-center">
-                            <div className="col-xl-9 col-lg-8 col-12">
-                                {
-                                    isLoading ? (
-                                        <div className='row'>
-                                            <div className="col-sm-12 col-xl-12">
-                                                <SkeletonTheme>
-                                                    <Skeleton height={300} />
-                                                </SkeletonTheme>
-                                            </div>
-                                            <div className="col-sm-12 col-xl-12">
-                                                <SkeletonTheme>
-                                                    <Skeleton height={100} />
-                                                </SkeletonTheme>
-                                            </div>
-                                        </div>
-                                    ) : (
-                                        <>
-                                            <div className="col-12 position-relative">
-                                                <iframe src={`https://www.youtube.com/embed/${demoEvents?.content?.url.split("?v=")[1]}`}
-                                                    title={demoEvents?.title}
-                                                    width="100%"
-                                                    height="500px"
-                                                    display="block"
-                                                    position="relative" />
-                                            </div>
-                                            <div className="col-lg-12">
-                                                <div className="bg-body shadow rounded-2 p-4">
-                                                    <h5 className="mb-3 ">{demoEvents?.title ? demoEvents.title : "Tidak ada judul."}</h5>
-                                                    <p className="mb-0">{demoEvents?.description ? demoEvents.description : "Tidak ada deskripsi."}</p>
-                                                </div>
-                                            </div>
-                                        </>
-                                    )
-                                }
-                            </div>
-                            {/* <div className="col-xl-4 col-lg-4 col-12">
-                                <div data-sticky data-margin-top="80" data-sticky-for="768">
-                                    <div className="card card-body shadow  p-4">
-                                        <div className="row g-5">
-                                            <div className="col-12">
-                                                <h5 className="mb-4">Vidio Lainnya</h5>
-                                                <div className="d-sm-flex justify-content-sm-between align-items-center">
-                                                    <div className="d-flex">
-                                                        <Link to="" className="btn btn-danger-soft btn-round mb-0">
-                                                            <i className="fas fa-play" />
-                                                        </Link>
-                                                        <Link to="" className="ms-2 ms-sm-3 mt-1 mt-sm-0 d-flex align-items-center">
-                                                            <h6 className="mb-0">Judul</h6>
-                                                        </Link>
-                                                    </div>
-                                                    <Link to="" className="btn btn-sm btn-success mb-0" >
-                                                        Buka
-                                                    </Link>
-                                                </div>
-                                                <hr />
-                                            </div>
+                {
+                    isLoading || Object.keys(demoEvents).length === 0 ? (
+                        <section className='bg-light py-0 py-sm-5'>
+                            <div className='container text-center' style={{ marginTop: '173px', marginBottom: '173px' }}>
+                                <div className='row'>
+                                    <div className='col-12'>
+                                        <div className='spinner-border' role='status'>
+                                            <span className='visually-hidden'>Loading...</span>
                                         </div>
                                     </div>
                                 </div>
-                            </div> */}
-                        </div>
-                    </div>
-                </section>
+                            </div>
+                        </section>
+                    ) : (
+                        <section className="pt-3 pt-xl-5">
+                            <div className="container" data-sticky-container>
+                                <div className="row g-4 justify-content-center">
+                                    <div className="col-xl-9 col-lg-8 col-12">
+                                        {
+                                            isLoading ? (
+                                                <div className='row'>
+                                                    <div className="col-sm-12 col-xl-12">
+                                                        <SkeletonTheme>
+                                                            <Skeleton height={300} />
+                                                        </SkeletonTheme>
+                                                    </div>
+                                                    <div className="col-sm-12 col-xl-12">
+                                                        <SkeletonTheme>
+                                                            <Skeleton height={100} />
+                                                        </SkeletonTheme>
+                                                    </div>
+                                                </div>
+                                            ) : (
+                                                <>
+                                                    <div className="col-12 position-relative">
+                                                        <iframe src={`https://www.youtube.com/embed/${demoEvents?.content?.url.split("?v=")[1]}`}
+                                                            title={demoEvents?.title}
+                                                            width="100%"
+                                                            height="500px"
+                                                            display="block"
+                                                            position="relative" />
+                                                    </div>
+                                                    <div className="col-lg-12">
+                                                        <div className="bg-body shadow rounded-2 p-4">
+                                                            <h5 className="mb-3 ">{demoEvents?.title ? demoEvents.title : "Tidak ada judul."}</h5>
+                                                            <p className="mb-0">{demoEvents?.description ? demoEvents.description : "Tidak ada deskripsi."}</p>
+                                                        </div>
+                                                    </div>
+                                                </>
+                                            )
+                                        }
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                    )
+                }
             </main>
             <Footer />
         </>
