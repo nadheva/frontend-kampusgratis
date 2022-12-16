d7210cb795cef379adc4f8f40e32d1977eb031e8
rizki1912 <rizki191220@gmail.com>
2022-12-16 16:43:53 +0700

    kumpulan chart

--- src/components/analitik/analitik/Chart.jsx
+++ src/components/analitik/analitik/Chart.jsx
@@ -1,273 +1,282 @@
-import React, { PureComponent, useCallback, useState } from 'react';
+import React from "react";
+import { render } from "react-dom";
+
+// Import react-circular-progressbar module and styles
 import {
-    LineChart,
-    Line,
-    XAxis,
-    YAxis,
-    CartesianGrid,
-    Tooltip,
-    Legend,
-    ResponsiveContainer,
-    ComposedChart,
-    Bar,
-    PieChart, Pie, Sector,
-} from "recharts";
+	CircularProgressbar,
+	CircularProgressbarWithChildren,
+	buildStyles,
+} from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
-// PieChart LineChart
-const data = [
-    {
-        name: "Semester 1",
-        semester: 2400,
-        amt: 2400
-    },
-    {
-        name: "Semester 2",
-        semester: 1398,
-        amt: 2210
-    },
-    {
-        name: "Semester 3",
-        semester: 9800,
-        amt: 2290
-    },
-    {
-        name: "Semester 4",
-        semester: 3908,
-        amt: 2000
-    },
-    {
-        name: "Semester 5",
-        semester: 4800,
-        amt: 2181
-    },
-    {
-        name: "Semester 6",
-        semester: 3800,
-        amt: 2500
-    },
-    {
-        name: "Semester 7",
-        semester: 4300,
-        amt: 2100
-    }
-];
+// Animation
+import { easeQuadInOut } from "d3-ease";
+import AnimatedProgressProvider from "../../../assets/js/AnimatedProgressProvider";
+import ChangingProgressProvider from "../../../assets/js/ChangingProgressProvider";
 
+// Radial separators
+import RadialSeparators from "../../../assets/js/RadialSeparators";
 
-// Data PieChart
-const data2 = [
-    { name: 'Group A', value: 400 },
-    { name: 'Group B', value: 300 },
-    { name: 'Group C', value: 300 },
-    { name: 'Group D', value: 200 },
-];
+const percentage = 66;
 
-const data3 = [
-    {
-        name: "Page A",
-        uv: 590,
-        pv: 800,
-        amt: 1400,
-        cnt: 490
-    },
-    {
-        name: "Page B",
-        uv: 868,
-        pv: 967,
-        amt: 1506,
-        cnt: 590
-    },
-    {
-        name: "Page C",
-        uv: 1397,
-        pv: 1098,
-        amt: 989,
-        cnt: 350
-    },
-    {
-        name: "Page D",
-        uv: 1480,
-        pv: 1200,
-        amt: 1228,
-        cnt: 480
-    },
-    {
-        name: "Page E",
-        uv: 1520,
-        pv: 1108,
-        amt: 1100,
-        cnt: 460
-    },
-    {
-        name: "Page F",
-        uv: 1400,
-        pv: 680,
-        amt: 1700,
-        cnt: 380
-    }
-];
+const Chart = () => (
+	<div style={{ padding: "40px 40px 40px 40px" }}>
+		<h1>react-circular-progressbar examples</h1>
+		<p>
+			<a href="https://github.com/kevinsqi/react-circular-progressbar">
+				<strong>View Github docs</strong>
+			</a>
+		</p>
 
+		<h2>Common style customizations</h2>
+		<Example label="Default">
+			<CircularProgressbar value={percentage} text={`${percentage}%`} />
+		</Example>
+		<Example label="Stroke width">
+			<CircularProgressbar
+				value={percentage}
+				text={`${percentage}%`}
+				strokeWidth={5}
+			/>
+		</Example>
+		<Example label="Square linecaps">
+			<CircularProgressbar
+				value={percentage}
+				text={`${percentage}%`}
+				styles={buildStyles({
+					strokeLinecap: "butt",
+				})}
+			/>
+		</Example>
+		<Example label="Custom colors">
+			<CircularProgressbar
+				value={percentage}
+				text={`${percentage}%`}
+				styles={buildStyles({
+					textColor: "red",
+					pathColor: "turquoise",
+					trailColor: "gold",
+				})}
+			/>
+		</Example>
+		<Example label="Text size">
+			<CircularProgressbar
+				value={percentage}
+				text={`${percentage}%`}
+				styles={buildStyles({
+					// This is in units relative to the 100x100px
+					// SVG viewbox.
+					textSize: "14px",
+				})}
+			/>
+		</Example>
+		<Example label="Rotation">
+			<CircularProgressbar
+				value={percentage}
+				text={`${percentage}%`}
+				styles={buildStyles({
+					rotation: 0.5 + (1 - percentage / 100) / 2,
+				})}
+			/>
+		</Example>
 
-// Start PieChart
-const renderActiveShape = (props) => {
-    const RADIAN = Math.PI / 180;
-    const { cx, cy, midAngle, innerRadius, outerRadius, startAngle, endAngle, fill, payload, percent, value } = props;
-    const sin = Math.sin(-RADIAN * midAngle);
-    const cos = Math.cos(-RADIAN * midAngle);
-    const sx = cx + (outerRadius + 10) * cos;
-    const sy = cy + (outerRadius + 10) * sin;
-    const mx = cx + (outerRadius + 30) * cos;
-    const my = cy + (outerRadius + 30) * sin;
-    const ex = mx + (cos >= 0 ? 1 : -1) * 22;
-    const ey = my;
-    const textAnchor = cos >= 0 ? 'start' : 'end';
+		<h2>Animation</h2>
+		<Example label="Default animation speed">
+			<ChangingProgressProvider values={[0, 20, 40, 60, 80, 100]}>
+				{(percentage) => (
+					<CircularProgressbar value={percentage} text={`${percentage}%`} />
+				)}
+			</ChangingProgressProvider>
+		</Example>
+		<Example label="Custom animation speed">
+			<ChangingProgressProvider values={[0, 20, 40, 60, 80, 100]}>
+				{(percentage) => (
+					<CircularProgressbar
+						value={percentage}
+						text={`${percentage}%`}
+						styles={buildStyles({
+							pathTransitionDuration: 0.15,
+						})}
+					/>
+				)}
+			</ChangingProgressProvider>
+		</Example>
+		<Example label="No animation when returning to 0">
+			<ChangingProgressProvider values={[0, 100]}>
+				{(percentage) => (
+					<CircularProgressbar
+						value={percentage}
+						text={`${percentage}%`}
+						styles={buildStyles({
+							pathTransition:
+								percentage === 0 ? "none" : "stroke-dashoffset 0.5s ease 0s",
+						})}
+					/>
+				)}
+			</ChangingProgressProvider>
+		</Example>
+		<Example label="Fully controlled text animation using react-move">
+			<AnimatedProgressProvider
+				valueStart={0}
+				valueEnd={66}
+				duration={1.4}
+				easingFunction={easeQuadInOut}
+				repeat
+			>
+				{(value) => {
+					const roundedValue = Math.round(value);
+					return (
+						<CircularProgressbar
+							value={value}
+							text={`${roundedValue}%`}
+							/* This is important to include, because if you're fully managing the
+        animation yourself, you'll want to disable the CSS animation. */
+							styles={buildStyles({ pathTransition: "none" })}
+						/>
+					);
+				}}
+			</AnimatedProgressProvider>
+		</Example>
 
-    return (
-        <g>
-            <text x={cx} y={cy} dy={8} textAnchor="middle" fill={fill}>
-                {payload.name}
-            </text>
-            <Sector
-                cx={cx}
-                cy={cy}
-                innerRadius={innerRadius}
-                outerRadius={outerRadius}
-                startAngle={startAngle}
-                endAngle={endAngle}
-                fill={fill}
-            />
-            <Sector
-                cx={cx}
-                cy={cy}
-                startAngle={startAngle}
-                endAngle={endAngle}
-                innerRadius={outerRadius + 6}
-                outerRadius={outerRadius + 10}
-                fill={fill}
-            />
-            <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} fill="none" />
-            <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
-            <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill="#333">{`PV ${value}`}</text>
-            <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} dy={18} textAnchor={textAnchor} fill="#999">
-                {`(Rate ${(percent * 100).toFixed(2)}%)`}
-            </text>
-        </g>
-    );
-};
-// End PieChart
+		<h2>Other use cases</h2>
+		<Example label="Arbitrary content">
+			<CircularProgressbarWithChildren value={66}>
+				{/* Put any JSX content in here that you'd like. It'll be vertically and horizonally centered. */}
+				<img
+					style={{ width: 40, marginTop: -5 }}
+					src="https://i.imgur.com/b9NyUGm.png"
+					alt="doge"
+				/>
+				<div style={{ fontSize: 12, marginTop: -5 }}>
+					<strong>66%</strong> mate
+				</div>
+			</CircularProgressbarWithChildren>
+		</Example>
+		<Example label="Multiple overlapping paths">
+			<CircularProgressbarWithChildren
+				value={80}
+				styles={buildStyles({
+					pathColor: "#f00",
+					trailColor: "#eee",
+					strokeLinecap: "butt",
+				})}
+			>
+				{/* Foreground path */}
+				<CircularProgressbar
+					value={70}
+					styles={buildStyles({
+						trailColor: "transparent",
+						strokeLinecap: "butt",
+					})}
+				/>
+			</CircularProgressbarWithChildren>
+		</Example>
+		<Example label="Multiple concentric paths">
+			<CircularProgressbarWithChildren
+				value={75}
+				strokeWidth={8}
+				styles={buildStyles({
+					pathColor: "#f00",
+					trailColor: "transparent",
+				})}
+			>
+				{/*
+          Width here needs to be (100 - 2 * strokeWidth)% 
+          in order to fit exactly inside the outer progressbar.
+        */}
+				<div style={{ width: "84%" }}>
+					<CircularProgressbar
+						value={70}
+						styles={buildStyles({
+							trailColor: "transparent",
+						})}
+					/>
+				</div>
+			</CircularProgressbarWithChildren>
+		</Example>
+		<Example label="Background">
+			<CircularProgressbar
+				value={percentage}
+				text={`${percentage}%`}
+				background
+				backgroundPadding={6}
+				styles={buildStyles({
+					backgroundColor: "#3e98c7",
+					textColor: "#fff",
+					pathColor: "#fff",
+					trailColor: "transparent",
+				})}
+			/>
+		</Example>
+		<Example label="Counterclockwise">
+			<CircularProgressbar
+				value={percentage}
+				text={`${percentage}%`}
+				counterClockwise
+			/>
+		</Example>
 
-const Chart = () => {
+		<Example label="Pie chart">
+			<CircularProgressbar
+				value={percentage}
+				strokeWidth={50}
+				styles={buildStyles({
+					strokeLinecap: "butt",
+				})}
+			/>
+		</Example>
+		<Example label="Progressbar with separators">
+			<CircularProgressbarWithChildren
+				value={80}
+				text={`${80}%`}
+				strokeWidth={10}
+				styles={buildStyles({
+					strokeLinecap: "butt",
+				})}
+			>
+				<RadialSeparators
+					count={12}
+					style={{
+						background: "#fff",
+						width: "2px",
+						// This needs to be equal to props.strokeWidth
+						height: `${10}%`,
+					}}
+				/>
+			</CircularProgressbarWithChildren>
+		</Example>
+		<Example label="Dashboard/speedometer">
+			<ChangingProgressProvider values={[0, 20, 80]}>
+				{(value) => (
+					<CircularProgressbar
+						value={value}
+						text={`${value}%`}
+						circleRatio={0.75}
+						styles={buildStyles({
+							rotation: 1 / 2 + 1 / 8,
+							strokeLinecap: "butt",
+							trailColor: "#eee",
+						})}
+					/>
+				)}
+			</ChangingProgressProvider>
+		</Example>
+	</div>
+);
 
-    // Start PieChart
-    const [activeIndex, setActiveIndex] = useState(0);
-    const onPieEnter = useCallback(
-        (_, index) => {
-            setActiveIndex(index);
-        },
-        [setActiveIndex]
-    );
-    // End PieChart
-
-
-
-    return (
-        <>
-            <section className="pt-4">
-                <div className="container">
-                    <div>
-                        <h3>Performa semester ini</h3>
-                    </div>
-                    <div className="card shadow">
-                        <div className="card-body">
-                            <PieChart width={500} height={400}>
-                                <Pie
-                                    activeIndex={activeIndex}
-                                    activeShape={renderActiveShape}
-                                    data={data2}
-                                    cx={200}
-                                    cy={200}
-                                    innerRadius={60}
-                                    outerRadius={80}
-                                    fill="#8884d8"
-                                    dataKey="value"
-                                    onMouseEnter={onPieEnter}
-                                />
-                            </PieChart>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <section className="pt-4">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-8 mb-4">
-                            <h3>Analisa hasil belajar tiap semester</h3>
-                        </div>
-                    </div>
-                    <div className="card shadow h-100">
-                        <div className="card-body pt-5">
-                            <ResponsiveContainer aspect={2.3}>
-                                <LineChart
-                                    width={1300}
-                                    height={300}
-                                    data={data}
-                                    margin={{
-                                        top: 5,
-                                        right: 30,
-                                        left: 20,
-                                        bottom: 5
-                                    }}
-                                >
-                                    <CartesianGrid strokeDasharray="3 3" />
-                                    <XAxis dataKey="name" />
-                                    <YAxis />
-                                    <Tooltip />
-                                    <Legend />
-                                    <Line
-                                        type="monotone"
-                                        dataKey="semester"
-                                        stroke="#8884d8"
-                                        activeDot={{ r: 8 }}
-                                    />
-                                </LineChart>
-                            </ResponsiveContainer>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <section className="pt-4">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-8 mb-4">
-                            <h3>Analisa performa mahasiswa</h3>
-                        </div>
-                    </div>
-                    <div className="card shadow h-100">
-                        <div className="card-body pt-5">
-                            <ComposedChart
-                                width={500}
-                                height={400}
-                                data={data3 }
-                                margin={{
-                                    top: 20,
-                                    right: 20,
-                                    bottom: 20,
-                                    left: 20
-                                }}
-                            >
-                                <CartesianGrid stroke="#f5f5f5" />
-                                <XAxis dataKey="name" scale="band" />
-                                <YAxis />
-                                <Tooltip />
-                                <Legend />
-                                <Bar dataKey="uv" barSize={20} fill="#413ea0" />
-                                <Line type="monotone" dataKey="uv" stroke="#ff7300" />
-                            </ComposedChart>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </>
-    )
+function Example(props) {
+	return (
+		<div style={{ marginBottom: 80 }}>
+			<hr style={{ border: "2px solid #ddd" }} />
+			<div style={{ marginTop: 30, display: "flex" }}>
+				<div style={{ width: "30%", paddingRight: 30 }}>{props.children}</div>
+				<div style={{ width: "70%" }}>
+					<h3 className="h5">{props.label}</h3>
+					<p>{props.description}</p>
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default Chart
+export default Chart;
