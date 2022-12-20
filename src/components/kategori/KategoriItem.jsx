953b5c581502f0835fc2d94b25447d89a9550afd
ary-bod <ibhodro123@gmail.com>
2022-12-20 13:39:20 +0700

    fix center icon and text & change card height

--- src/components/kategori/KategoriItem.jsx
+++ src/components/kategori/KategoriItem.jsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom'
 
 const KategoriItem = ({ category }) => {
     return (
-        <div className="col-sm-6 col-md-4 col-xl-3" key={category.id}>
-            <div className={`card card-body ${category.disabled ? "bg-dark" : category.background} bg-opacity-10 text-center position-relative btn-transition p-4`}>
+        <div className="col-sm-6 col-xl-3" key={category.id}>
+            <div className={`card card-body ${category.disabled ? "bg-dark" : category.background} bg-opacity-10 text-center position-relative btn-transition p-4 h-100 justify-content-center`}>
                 <div className="icon-xl bg-body mx-auto rounded-circle mb-3">
                     <img
                         loading="lazy"
